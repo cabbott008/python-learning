cc3bf84f55bd8092fa21c32e58ddf68660b60cf0
ca <curtisabbott@me.com>
2023-05-09 01:22:53 -0500

    small change

--- ex11.py
+++ ex11.py
@@ -3,8 +3,8 @@ x = open('mbox.txt')
 count = 0
 for line in x:
     line = line.strip()
-    #lst = re.findall('[a-zA-Z0-9]\S*@\S*[a-zA-Z]', line)
-    #if len(lst) > 0:
+    lst = re.findall('[a-zA-Z0-9]\S*@\S*[a-zA-Z]', line)
+    if len(lst) > 0:
     y = re.findall('^From .* ([0-9][0-9]):', line)
     if len(y) > 0:
         count += 1
