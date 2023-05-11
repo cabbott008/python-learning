921f06b429b080e44a73e0c36e96f03ca1ebf3a2
ca <curtisabbott@me.com>
2023-05-11 01:58:05 -0500

    Test

--- ex11.py
+++ ex11.py
@@ -5,8 +5,8 @@ for line in x:
     line = line.strip()
     lst = re.findall('[a-zA-Z0-9]\S*@\S*[a-zA-Z]', line)
     if len(lst) > 0:
-    y = re.findall('^From .* ([0-9][0-9]):', line)
-    if len(y) > 0:
+    # y = re.findall('^From .* ([0-9][0-9]):', line)
+    # if len(y) > 0:
         count += 1
         print(y)
 print(count)
--- ex11.test.py
+++ ex11.test.py
@@ -1,15 +1,18 @@
 import re
 # searcher = input('Please enter RegEx: ')
 x = open('regex2.txt')
-count = 0
-totals = 0
-lst = None
+#totals = 0
 
-for line in x:
-    line = line.strip()
-    lst = re.findall('([0-9]+)', line)
-    if len(lst) > 0:
-        totals = totals + float(lst[0])
-        count += 1
-print(totals)
-print(count)
+#for line in x:
+#    line = line.strip()
+#    line = re.findall('([0-9]+)', line)
+#    for word in line:
+#        if len(word) > 0:
+#            totals = totals +int(word)
+#print(totals)
+
+print( sum( [int(i) for i in re.findall('[0-9]+', x.read())]))
+
+#lst = re.findall('[0-9]+', x.read())
+#ilst = [int(i) for i in lst]
+#print(sum(ilst))
